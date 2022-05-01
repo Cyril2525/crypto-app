1b0497bc76d1c44091652444eceda16939943e44
Cyril2525 <chinemeremc25@gmail.com>
2022-05-01 10:45:39 -0700

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,32 @@
+<div class="footer">
+         <div class="footer1">
+             <div class="footer_column">
+                 <h3>C-HUB EXCHANGE</h3>
+                 <p class="p">C-HUB EXCHANGE delivers swift, excellent service that meets and exceeds the service expectations of her esteemed clients with lightening speed to maximise client satisfaction.</p> 
+             </div>
+
+             <div class="footer_column">
+             <h3>CONTACT INFO</h3>
+                 <p class="p">Office Phone: (234) 090-3324-6432<br>
+                 Coustomer service: (234) 081-6748-2198 <br>
+                 Email: <a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCKCFTSqNHGsxdzMvpxJLRHNRxWsZfxBqsGqTFSdLvgvsZgkjdpzwlwZqlQJwrCJDrplGGSq">chubexchange@gmail.com</a><br>
+                
+                </p> 
+             </div>
+
+             <div class="footer_column">
+             <h3>SERVICE HOURS</h3>
+                 <p class="p">Monday - Sunday: 08:00AM - 09:00PM
+                   For hours beyond 9pm and Weekends:call (234) 081-6748-2198</p>
+             </div>
+
+             <div class="footer_column">
+             <h3>SOCIALS</h3>
+                 <pre>
+                     <p class="p"><a href="https://wa.me/+2349033246432"><i class="fa-brands fa-whatsapp fa-3x fa-color"></i></a>  <a href="https://www.instagram.com/c_hub.exchange?r=nametag"><i class="fa-brands fa-instagram fa-3x fa-color1"></i></a>   <a href="https://twitter.com/C_HUB__Explorer?t=cyyDb3v5aH5yEgRjqQ3agA&s=09"><i class="fa-brands fa-twitter fa-3x fa-color2"></i></a></p>
+             </div>
+
+         </div>
+     <div class="footer2"><span>All Rights REserved &copy;2022 Designed By <a href="https://morrhtechsolutions.com/">Morrhtech Solutions</a></span></div>
+
+     </div>
---
+++ product.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require "includes/header1.php"; ?>
+<body>
+<?php require "includes/header2.php"; ?>
+<div class="title">PRODUCT</div>
+<div class="main_container">
+    <div class="product_sidebar">
+   
+    </div>
+    <div class="main_body">Our Products
+
+    <div class="product_container">
+            <div class="product">
+                <img src="images/product/BITCOIN.jpg" alt="">
+                <h4>Bitcoin</h4>
+            </div>
+            <div class="product">
+                <img src="images/product/Binance coin.webp" alt="">
+                <h4>BNB</h4>
+            </div>
+            <div class="product">
+                <img src="images/eth.png" alt="">
+                <h4>Etherieum</h4>
+            </div>
+            <div class="product">
+                <img src="images/product/usdt.jpg" alt="">
+                <h4>USDT</h4>
+            </div>
+        </div>
+
+        <div class="product_container">
+            <div class="product">
+                <img src="images/product/solana coin.jpeg" alt="">
+                <h4>Solana</h4>
+            </div>
+            <div class="product">
+                <img src="images/product/litecoin.png" alt="">
+                <h4>Litecoin</h4>
+            </div>
+            <div class="product">
+                <img src="images/product/busd.jpg" alt="">
+                <h4>BUSD</h4>
+            </div>
+            <div class="product">
+                <img src="images/product/cardano.jpg" alt="">
+                <h4>Cardano</h4>
+            </div>
+        </div>
+    </div>
+</div>
+     <?php include "includes/footer.php"; ?>
+
+     <script src="javascripts/script1.js"></script>
+</body>
+</html>
